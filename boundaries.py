11f15636df6e1dd672404a1e82f4d9ed8ed99fc8
AtomThum <atom.olee@gmail.com>
2024-10-25 12:51:22 +0700

    Added animations

--- boundaries.py
+++ boundaries.py
@@ -9,8 +9,12 @@ class WallBoundary:
         self.yResolution = yResolution
         self.invert = invert  # True if boundary, False if not
         self.boundary = np.full((xResolution, yResolution), invert)
+        self.invertedBoundary = np.invert(self.boundary)
         self.boundaryIndex = None
 
+    def updateInvertedBoundary(self):
+        self.invertedBoundary = np.invert(self.boundary)
+        
     def generateIndex(self):
         self.boundaryIndex = []
         for i, j in itr.product(range(self.xResolution), range(self.yResolution)):
@@ -18,12 +22,14 @@ class WallBoundary:
                 self.boundaryIndex.append((i, j))
             else:
                 pass
+        self.updateInvertedBoundary()
 
-    def cylindricalWall(self, cylinderCenter: list, cylinderRadius):
+    def cylindricalWall(self, cylinderCenter: list, cylinderRadius: float):
         for xIndex in range(self.xResolution):
             for yIndex in range(self.yResolution):
                 if math.dist(cylinderCenter, [xIndex, yIndex]) <= cylinderRadius:
                     self.boundary[xIndex, yIndex] = not self.invert
+        self.updateInvertedBoundary()
 
     def filledStraightRectangularWall(self, cornerCoord1: list, cornerCoord2: list):
         maxX = max(cornerCoord1[0], cornerCoord2[0])
@@ -40,9 +46,20 @@ class WallBoundary:
                     and (yIndex > minY)
                 ):
                     self.boundary[xIndex, yIndex] = not self.invert
+        self.updateInvertedBoundary()
+
+    # Border around the simulation
+    def borderWall(self):
+        self.boundary[0 : self.xResolution, 0] = not self.invert
+        self.boundary[0 : self.xResolution, self.yResolution - 1] = not self.invert
+        self.boundary[0, 0 : self.yResolution] = not self.invert
+        self.boundary[self.xResolution - 1, 0 : self.yResolution] = not self.invert
+        self.updateInvertedBoundary()
 
 
 indices = [[1, 8, 5], [2, 5, 6], [3, 6, 7], [4, 7, 8]]
+
+
 class PressureBoundary:
     def __init__(self, x: int, y: int, ux, uy, direction: int):
         self.x = x
@@ -58,3 +75,10 @@ class PressureBoundary:
         self.minorVelocity = uy if direction in [1, 3] else ux
         self.setIndices = indices[direction - 1]
         self.getIndices = indices[reflectIndex - 1]
+
+class VelocityBoundary:
+    def __init__(self, x:int, y:int, ux, uy):
+        self.x = x
+        self.y = y
+        self.ux = ux
+        self.uy = uy
