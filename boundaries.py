fdf18985b5ab2c37e7640a6342fbe48f09df6689
AtomThum <atom.olee@gmail.com>
2024-10-26 23:39:35 +0700

    Rebuilt the entire framework. Pressure boundary condition left to be done.

--- boundaries.py
+++ boundaries.py
@@ -6,11 +6,11 @@ np.seterr(divide=None, invalid=None)
 
 
 class WallBoundary:
-    def __init__(self, xResolution: int, yResolution: int, invert: bool = False):
-        self.xResolution = xResolution
+    def __init__(self, yResolution: int, xResolution: int, invert: bool = False):
         self.yResolution = yResolution
+        self.xResolution = xResolution
         self.invert = invert  # True if boundary, False if not
-        self.boundary = np.full((xResolution, yResolution), invert)
+        self.boundary = np.full((yResolution, xResolution), invert)
         self.invertedBoundary = np.invert(self.boundary)
         self.boundaryIndex = None
 
@@ -19,7 +19,7 @@ class WallBoundary:
 
     def generateIndex(self):
         self.boundaryIndex = []
-        for i, j in itr.product(range(self.xResolution), range(self.yResolution)):
+        for i, j in itr.product(range(self.yResolution), range(self.xResolution)):
             if self.boundary[i, j] != self.invert:
                 self.boundaryIndex.append((i, j))
             else:
@@ -27,45 +27,48 @@ class WallBoundary:
         self.updateInvertedBoundary()
 
     def cylindricalWall(self, cylinderCenter: list, cylinderRadius: float):
-        for xIndex in range(self.xResolution):
-            for yIndex in range(self.yResolution):
-                if math.dist(cylinderCenter, [xIndex, yIndex]) <= cylinderRadius:
-                    self.boundary[xIndex, yIndex] = not self.invert
+        for yIndex, xIndex in itr.product(
+            range(self.yResolution), range(self.xResolution)
+        ):
+            if math.dist(cylinderCenter, [yIndex, xIndex]) <= cylinderRadius:
+                self.boundary[yIndex, xIndex] = not self.invert
         self.updateInvertedBoundary()
 
-    def filledStraightRectangularWall(self, cornerCoord1: list, cornerCoord2: list):
-        maxX = max(cornerCoord1[0], cornerCoord2[0])
-        minX = min(cornerCoord1[0], cornerCoord2[0])
+    def filledStraightRectangularWall(self, cornerCoord1: tuple, cornerCoord2: tuple):
+        maxY = max(cornerCoord1[0], cornerCoord2[0])
+        minY = min(cornerCoord1[0], cornerCoord2[0])
+        maxX = max(cornerCoord1[1], cornerCoord2[1])
+        minX = min(cornerCoord1[1], cornerCoord2[1])
 
-        maxY = max(cornerCoord1[1], cornerCoord2[1])
-        minY = min(cornerCoord1[1], cornerCoord2[1])
-        for xIndex in range(self.xResolution):
-            for yIndex in range(self.yResolution):
-                if (
-                    (xIndex < maxX)
-                    and (xIndex > minX)
-                    and (yIndex < maxY)
-                    and (yIndex > minY)
-                ):
-                    self.boundary[xIndex, yIndex] = not self.invert
+        for yIndex, xIndex in itr.product(
+            range(self.yResolution), range(self.xResolution)
+        ):
+            if (
+                (xIndex <= maxX)
+                and (xIndex >= minX)
+                and (yIndex <= maxY)
+                and (yIndex >= minY)
+            ):
+                self.boundary[yIndex, xIndex] = not self.invert
         self.updateInvertedBoundary()
 
     # Border around the simulation
     # Thickness will be implemented later.
     def borderWall(self, thickness: int = 1):
-        self.boundary[0 : self.xResolution, -1 + thickness] = not self.invert
-        self.boundary[0 : self.xResolution, self.yResolution - thickness] = (
+        self.boundary[0 : self.yResolution, -1 + thickness] = not self.invert
+        self.boundary[0 : self.yResolution, self.xResolution - thickness] = (
             not self.invert
         )
-        self.boundary[-1 + thickness, 0 : self.yResolution] = not self.invert
-        self.boundary[self.xResolution - thickness, 0 : self.yResolution] = (
+        self.boundary[-1 + thickness, 0 : self.xResolution] = not self.invert
+        self.boundary[self.yResolution - thickness, 0 : self.xResolution] = (
             not self.invert
         )
         self.updateInvertedBoundary()
-    
-    def dotWall(self, *args: tuple):
+
+    # By numpy's convention, it's (0, 1)
+    def dotWalls(self, *args: tuple):
         for position in args:
-            self.boundary[position] = not self.invert
+            self.boundary[position[0], position[1]] = not self.invert
 
 
 class PressureBoundary:
@@ -89,8 +92,8 @@ class PressureBoundary:
 
 
 class VelocityBoundary:
-    def __init__(self, x: int, y: int, magnitude: list, direction: list):
-        self.x = x
+    def __init__(self, y: int, x: int, magnitude: tuple, direction: tuple):
         self.y = y
-        self.magnitude = magnitude
+        self.x = x
+        self.magnitude = magnitude # Follows the e conventions
         self.direction = np.array(direction)
--- simulation.py
+++ simulation.py
@@ -1,10 +1,7 @@
 import numpy as np
 import copy
+import itertools as itr
 from boundaries import WallBoundary, PressureBoundary, VelocityBoundary
-
-latticeSize = 9
-xResolution = 40
-yResolution = 40
 
 
 class Simulation:
@@ -17,70 +14,94 @@ class Simulation:
         [4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]
     )
     latticeSize = 9
+    reflectIndices = {0: 0, 1: 3, 2: 4, 3: 1, 4: 2, 5: 7, 6: 8, 7: 5, 8: 6}
 
     def __init__(
         self,
-        xResolution: int,
         yResolution: int,
+        xResolution: int,
         initCondition: np.array,
         wallBoundary: WallBoundary,
-        pressureBoundary: list = [],
-        velocityBoundary: list = [],
+        pressureBoundaries: list = [],
+        velocityBoundaries: list = [],
         relaxationTime: float = 0.809,
         initialStep: int = 0,
     ):
-        self.xResolution = xResolution
+        # By python's convention, yResolution must come first.
+        # The array must be of shape (yResolution, xResolution, Simulation.latticeSize)
         self.yResolution = yResolution
-        self.xIndex = np.arange(xResolution)
+        self.xResolution = xResolution
         self.yIndex = np.arange(yResolution)
+        self.xIndex = np.arange(xResolution)
 
-        self.fluid = initCondition  # Boundary is imposed by default.
+        self.initCondition = initCondition
+
+        self.fluid = initCondition
+        self.lastStepFluid = initCondition
         self.relaxationTime = relaxationTime
         self.wallBoundary = wallBoundary
-        self.pressureBoundaries = pressureBoundary
-        self.velocityBoundary = velocityBoundary
+        self.wallBoundary.generateIndex()
+        self.fluid[self.wallBoundary.boundary, :] = 0
+        self.pressureBoundaries = pressureBoundaries
+        self.velocityBoundaries = velocityBoundaries
         self.step = initialStep
 
-        self.fluid[self.wallBoundary.boundary, :] = 0
-        self.initialCondition = copy.deepcopy(self.fluid)
-
-        # Initial calculation from the initial condition. These variables will be updated as time goes on
         self.density = np.sum(self.fluid, axis=2)
-        self.momentumX = np.sum(self.fluid * Simulation.unitX, axis=2)
         self.momentumY = np.sum(self.fluid * Simulation.unitY, axis=2)
-        self.speedX = self.momentumX / self.density
+        self.momentumX = np.sum(self.fluid * Simulation.unitX, axis=2)
         self.speedY = self.momentumY / self.density
-        self.speedX = np.nan_to_num(self.speedX, posinf=0, neginf=0, nan=0)
+        self.speedX = self.momentumX / self.density
         self.speedY = np.nan_to_num(self.speedY, posinf=0, neginf=0, nan=0)
-
-        self.initialDensity = copy.deepcopy(self.density)
-        self.initialMomentumX = copy.deepcopy(self.momentumX)
-        self.initialMomentumY = copy.deepcopy(self.momentumY)
-        self.initialSpeedX = copy.deepcopy(self.speedX)
-        self.initialSpeedY = copy.deepcopy(self.speedY)
+        self.speedX = np.nan_to_num(self.speedX, posinf=0, neginf=0, nan=0)
 
     def updateDensity(self):
         self.density = np.sum(self.fluid, axis=2)
 
     def updateMomentum(self):
-        self.momentumX = np.sum(self.fluid * Simulation.unitX, axis=2)
         self.momentumY = np.sum(self.fluid * Simulation.unitY, axis=2)
+        self.momentumX = np.sum(self.fluid * Simulation.unitX, axis=2)
 
     # If this function is called, you don't have to update density and momentum
     def updateSpeed(self):
         self.updateDensity()
         self.updateMomentum()
 
-        self.speedX = self.momentumX / self.density
         self.speedY = self.momentumY / self.density
-        self.speedX = np.nan_to_num(self.speedX, posinf=0, neginf=0, nan=0)
+        self.speedX = self.momentumX / self.density
         self.speedY = np.nan_to_num(self.speedY, posinf=0, neginf=0, nan=0)
+        self.speedX = np.nan_to_num(self.speedX, posinf=0, neginf=0, nan=0)
 
-    def stepFluid(self):
-        # Equilizing step
+    def streamFluid(self):
+        for latticeIndex, shiftY, shiftX in zip(
+            range(Simulation.latticeSize), Simulation.unitY, Simulation.unitX
+        ):
+            self.fluid[:, :, latticeIndex] = np.roll(
+                self.fluid[:, :, latticeIndex], shiftY, axis=0
+            )
+            self.fluid[:, :, latticeIndex] = np.roll(
+                self.fluid[:, :, latticeIndex], shiftX, axis=1
+            )
+        self.updateSpeed()
+
+    def bounceBackFluid(self):
+        for y, x in self.wallBoundary.boundaryIndex:
+            for latticeIndex in range(Simulation.latticeSize):
+                if self.fluid[y, x, latticeIndex] != 0:
+                    self.fluid[
+                        y - Simulation.unitY[latticeIndex],
+                        x - Simulation.unitX[latticeIndex],
+                        Simulation.reflectIndices[latticeIndex],
+                    ] = self.fluid[y, x, latticeIndex]
+                    self.fluid[y, x, latticeIndex] = 0
+        self.updateSpeed()
+
+    def collideFluid(self):
         fluidEquilibrium = np.zeros(self.fluid.shape)
-        for latticeIndex, cx, cy, w in zip(
-            range(latticeSize), Simulation.unitX, Simulation.unitY, Simulation.weight
+        for latticeIndex, cy, cx, w in zip(
+            range(Simulation.latticeSize),
+            Simulation.unitY,
+            Simulation.unitX,
+            Simulation.weight,
         ):
             fluidEquilibrium[:, :, latticeIndex] = (
                 self.density
@@ -92,122 +113,40 @@ class Simulation:
                     - 3 * (self.speedX**2 + self.speedY**2) / 2
                 )
             )
-
-        # self.fluid[self.wallBoundary.invertedBoundary, :] -= (
-        #     self.fluid[self.wallBoundary.invertedBoundary, :]
-        #     - fluidEquilibrium[self.wallBoundary.invertedBoundary, :]
-        #     / self.relaxationTime
-        # )
-        self.fluid -= (self.fluid - fluidEquilibrium) / self.relaxationTime
-
-        for latticeIndex, shiftX, shiftY in zip(
-            range(latticeSize), Simulation.unitX, Simulation.unitY
-        ):
-            self.fluid[:, :, latticeIndex] = np.roll(
-                self.fluid[:, :, latticeIndex], shiftX, axis=1
-            )
-            self.fluid[:, :, latticeIndex] = np.roll(
-                self.fluid[:, :, latticeIndex], shiftY, axis=0
-            )
-
-        fluidBoundary = self.fluid[self.wallBoundary.boundary, :]
-        fluidBoundary = fluidBoundary[:, [0, 3, 4, 1, 2, 7, 8, 5, 6]]
-        self.fluid[self.wallBoundary.boundary, :] = fluidBoundary
-
-        for pressureBoundary in self.pressureBoundaries:
-            for latticeIndex in range(latticeSize):
-                self.fluid[pressureBoundary.x, pressureBoundary.y, latticeIndex] = 0
-            densityAtIndex = self.density[pressureBoundary.x, pressureBoundary.y]
-            self.fluid[
-                pressureBoundary.x, pressureBoundary.y, pressureBoundary.setIndices[0]
-            ] = self.fluid[
-                pressureBoundary.x, pressureBoundary.y, pressureBoundary.getIndices[0]
-            ] + (
-                2 / 3
-            ) * (
-                pressureBoundary.mainVelocity
-            )
-            self.fluid[
-                pressureBoundary.x, pressureBoundary.y, pressureBoundary.setIndices[1]
-            ] = (
-                self.fluid[
-                    pressureBoundary.x,
-                    pressureBoundary.y,
-                    pressureBoundary.getIndices[1],
-                ]
-                - (
-                    0.5
-                    * (
-                        self.fluid[
-                            pressureBoundary.x,
-                            pressureBoundary.y,
-                            (
-                                4
-                                if pressureBoundary.direction - 1 == 0
-                                else pressureBoundary.direction - 1
-                            ),
-                        ]
-                        - self.fluid[
-                            pressureBoundary.x,
-                            pressureBoundary.y,
-                            (
-                                1
-                                if pressureBoundary.direction + 1 == 5
-                                else pressureBoundary.direction + 1
-                            ),
-                        ]
-                    )
-                )
-                + (0.5 * densityAtIndex * pressureBoundary.minorVelocity)
-                + (1 / 6 * densityAtIndex * pressureBoundary.mainVelocity)
-            )
-            self.fluid[
-                pressureBoundary.x, pressureBoundary.y, pressureBoundary.setIndices[2]
-            ] = (
-                self.feluid[
-                    pressureBoundary.x,
-                    pressureBoundary.y,
-                    pressureBoundary.getIndices[2],
-                ]
-                + (
-                    0.5
-                    * (
-                        self.fluid[
-                            pressureBoundary.x,
-                            pressureBoundary.y,
-                            (
-                                4
-                                if pressureBoundary.direction - 1 == 0
-                                else pressureBoundary.direction - 1
-                            ),
-                        ]
-                        - self.fluid[
-                            pressureBoundary.x,
-                            pressureBoundary.y,
-                            (
-                                1
-                                if pressureBoundary.direction + 1 == 5
-                                else pressureBoundary.direction + 1
-                            ),
-                        ]
-                    )
-                )
-                - (0.5 * densityAtIndex * pressureBoundary.minorVelocity)
-                + (1 / 6 * densityAtIndex * pressureBoundary.mainVelocity)
-            )
-
+        self.fluid += (fluidEquilibrium - self.fluid) / self.relaxationTime
         self.updateSpeed()
-        self.step += 1
-        return self.fluid
 
-    def simulateFluid(self, step: int):
-        [self.stepFluid() for _ in range(step)]
+    def imposeVelocityBoundaryCondition(self):
+        for velocityBoundary in self.velocityBoundaries:
+            self.fluid[
+                velocityBoundary.y, velocityBoundary.x, velocityBoundary.direction
+            ] = velocityBoundary.magnitude
+        self.updateSpeed()
 
-    # Uses mean absolute error to determine whether the fluid is in equilibrium
+    def stepSimulation(self):
+        self.lastStepFluid = self.fluid
+        self.streamFluid()
+        self.bounceBackFluid()
+        self.collideFluid()
+        self.imposeVelocityBoundaryCondition()
+
+    def simulate(self, step: int = 1):
+        [self.stepSimulation() for _ in range(step)]
+
     def isAtDensityEquilibirum(self, threshold: float):
-        meanDensity = np.mean(self.density)
-        error = np.sum(np.abs(self.density - meanDensity)) / (xResolution * yResolution)
+        error = np.sum(np.abs(self.lastStepFluid - self.fluid)) / (
+            self.xResolution * self.yResolution
+        )
         if error > threshold:
             return False
         else:
             return True
+
+    def simulateUntilEquilibrium(self, limit: int = 5000, threshold: float = 0.5):
+        for _ in range(limit):
+            self.stepSimulation()
+            if self.isAtDensityEquilibirum(threshold):
+                break
+            else:
+                pass
+            step += 1
