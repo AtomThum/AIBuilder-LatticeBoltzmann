1b3e4781cd7abc31ff0b890591c78164c36ab829
KOguy64 <Rainkunakorn@gmail.com>
2024-10-27 14:30:57 +0700

    Able to randomly generate room layout, as well as pressure boundary positions and directions

--- boundaries.py
+++ boundaries.py
@@ -1,11 +1,19 @@
 import numpy as np
 import math
 import itertools as itr
+from scipy.ndimage import convolve
 
 np.seterr(divide=None, invalid=None)
 
+def numerical_inverse(n):
+    return int(n != 1)
 
 class WallBoundary:
+    mineSweeper = [[1,1,1],[1,0,1],[1,1,1]]
+    unitVect = np.array(
+        [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]]
+    )
+
     def __init__(self, yResolution: int, xResolution: int, invert: bool = False):
         self.yResolution = yResolution
         self.xResolution = xResolution
@@ -13,6 +21,8 @@ class WallBoundary:
         self.boundary = np.full((yResolution, xResolution), invert)
         self.invertedBoundary = np.invert(self.boundary)
         self.boundaryIndex = None
+        self.possibleACPos = None
+        self.possibleACDirections = None
 
     def updateInvertedBoundary(self):
         self.invertedBoundary = np.invert(self.boundary)
@@ -25,6 +35,27 @@ class WallBoundary:
             else:
                 pass
         self.updateInvertedBoundary()
+
+    def generateACPosandDirections(self):
+        self.possibleACPos = []
+        self.possibleACDirections = []
+        applyall = np.vectorize(numerical_inverse)
+        inverted = applyall(self.boundary.astype(int))
+        padded_array = np.pad(inverted, pad_width=1, mode='constant', constant_values=0)
+        edgeSwept = convolve(padded_array, WallBoundary.mineSweeper)[1:-1, 1:-1] 
+        edgesProcessed = np.logical_and(edgeSwept >= 2, edgeSwept <= 5)
+
+        for i in self.boundaryIndex:
+            if (edgesProcessed[i[0], i[1]]):
+                self.possibleACPos.append(i)
+                possibleACDirection = []
+                for index, j in enumerate(WallBoundary.unitVect):
+                    if (i[0] + j[0] >= 0 and i[0] + j[0] < self.yResolution and i[1] + j[1] >= 0 and i[1] + j[1] < self.xResolution):
+                        isWallThere = self.boundary[i[0] + j[0], i[1] + j[1]]
+                        if (not isWallThere):
+                            possibleACDirection.append(index)
+                self.possibleACDirections.append(possibleACDirection)
+
 
     def cylindricalWall(self, cylinderCenter: list, cylinderRadius: float):
         for yIndex, xIndex in itr.product(
